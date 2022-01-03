d36c107f84b5a3c1dbb928ca7318f32c656b7e7a
Aldino Efendi <aldino.efendi7@gmail.com>
2022-01-03 23:04:42 +0700

    Initial Commit

---
+++ coding-interview/coding_interviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  coding_interviewApp.swift
+//  coding-interview
+//
+//  Created by Aldino Efendi on 2022/01/03.
+//
+
+import SwiftUI
+
+@main
+struct coding_interviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
